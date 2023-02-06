0552ab55c001f84afaf924890af53f0a94884e37
mir-hussain <mirhussainmurtaza@gmail.com>
2023-02-06 20:31:42 +0600

    half pyramid while and do while loop added

--- half-pyramid/main.c
+++ half-pyramid/main.c
@@ -2,8 +2,10 @@
 
 main()
 {
-    int i, j;
 
+    //*  Using for loop
+
+    int i, j;
     for (i = 0; i <= 5; i++)
     {
         for (j = 0; j <= i; j++)
@@ -12,4 +14,38 @@ main()
         }
         printf("\n");
     }
+
+    //* Using while loop
+
+    // int i = 0, j = 0;
+
+    // while (i <= 5)
+    // {
+    //     j = 0;
+    //     while (j <= i)
+    //     {
+    //         printf("*");
+    //         j++;
+    //     }
+    //     printf("\n");
+
+    //     i++;
+    // }
+
+    //* Using do...while loop
+
+    // int i = 0, j = 0;
+
+    // do
+    // {
+    //     j = 0;
+    //     while (j <= i)
+    //     {
+    //         printf("*");
+    //         j++;
+    //     }
+    //     printf("\n");
+
+    //     i++;
+    // } while (i <= 5);
 }
